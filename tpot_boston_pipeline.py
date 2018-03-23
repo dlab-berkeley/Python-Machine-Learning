1ad36a3277b8960b927f8b5b616d656d204cb1c5
Christopher Gagne <cgagne@berkeley.edu>
2018-03-23 15:49:42 -0700

    chris g updated a few things after class

--- tpot_boston_pipeline.py
+++ tpot_boston_pipeline.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.svm import LinearSVR
 from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
@@ -12,10 +12,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-10.807116840972014
+# Score on the training set was:-14.385480619108217
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=False, max_features=0.8500000000000001, min_samples_leaf=3, min_samples_split=3, n_estimators=100)),
-    LinearSVR(C=1.0, dual=False, epsilon=0.001, loss="squared_epsilon_insensitive", tol=0.001)
+    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=True, max_features=0.7500000000000001, min_samples_leaf=6, min_samples_split=2, n_estimators=100)),
+    StackingEstimator(estimator=RidgeCV()),
+    ExtraTreesRegressor(bootstrap=True, max_features=0.4, min_samples_leaf=1, min_samples_split=15, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
--- tpot_iris_pipeline.py
+++ tpot_iris_pipeline.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.kernel_approximation import RBFSampler
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator
+from sklearn.pipeline import make_pipeline
+from sklearn.tree import DecisionTreeClassifier
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -14,8 +13,8 @@ training_features, testing_features, training_target, testing_target = \
 
 # Score on the training set was:0.9826086956521738
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=LogisticRegression(C=10.0, dual=True, penalty="l2")),
-    GaussianNB()
+    RBFSampler(gamma=0.65),
+    DecisionTreeClassifier(criterion="entropy", max_depth=2, min_samples_leaf=14, min_samples_split=11)
 )
 
 exported_pipeline.fit(training_features, training_target)
